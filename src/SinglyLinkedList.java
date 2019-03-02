aeea6050136abe77c972f09564ea5c438682d8eb
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-03-02 11:49:59 +0530

    write specifications for the class

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -1,2 +1,14 @@
 public class SinglyLinkedList {
+
+  /*
+  * Specifications for the Singly Linked List Class:
+  * 1. A pointer to the first node of the list called 'Head' of 'Node' type.
+  * 2. A variable to store the current number of node in the list called 'size'.
+  * 3. A specification class for the 'Node' type for the SinglyLinkedList
+  * 4. Private helper methods for the methods
+  * 5. Public methods for the SinglyLinkedList class to serve as the functions of the list.
+  * */
+
+
+
 }
